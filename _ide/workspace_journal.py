9a102d3b2107b3c47cd3a8f93a7e848ff72caedc
raulest50 <raulest50@gmail.com>
2025-07-13 16:14:03 -0500

    se agrego test bench file

---
+++ _ide/workspace_journal.py
@@ -0,0 +1,9 @@
+# 2025-07-13T16:00:53.358111700
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="z_scan_acceleration_ovr")
+
+comp = client.get_component(name="propagation_kernel_v1")
+comp.run(operation="C_SIMULATION")
+
